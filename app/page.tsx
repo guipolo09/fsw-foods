b5914192fcb3a3fac815619f3853a9536931869a
guipolo09 <guilhermemartinspolo@gmail.com>
2024-05-01 15:14:56 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "./_components/category_list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -11,6 +12,17 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza"
+          height={0}
+          width={0}
+          className="object-caontain h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
